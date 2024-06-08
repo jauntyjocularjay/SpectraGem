1bf258d5edc382e6c160c9df6ea1ffd4115966c6
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-06-08 04:38:39 -0700

    added layers of gems on board

--- components/Scenes.mjs
+++ components/Scenes.mjs
@@ -47,26 +47,28 @@ MAIN.preload = function() {
 }
 
 MAIN.create = function() {
+    let temp = {}
     /** @object background is the background grid */
     background = this.add.sprite(VIEW.width / 2, VIEW.height / 2, '6x8')
+
+    /** @object platforms hold the gems up */
+    platform = this.physics.add.staticGroup()
+    platform.create(VIEW.width / 2, VIEW.height-30, 'platformx384')
 
     for(let j = 0; j < 8; j++){
         for(let i = 4; i < 10; i++){
             gems = this.physics.add.group({
                 key: Gem.random().color.alias,
-                repeat: 0,
-                setXY: { x: i * VIEW.width/13, y: 0, stepX: 64}
+                setXY: { x: i * VIEW.width/13, y: 64 * j},
+                // customBoundsRectangle: {x:64, y:0, width: 64, height: 64}
             })
+            gems.children.iterate(function(child){
+                child.setBounceY(Phaser.Math.FloatBetween(0.3, 0.9))
+            })
+            this.physics.add.collider(gems, platform)
         }
-        gems.children.iterate(function(child){
-            child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.3))
-        })
     }
-
-    /** @object platforms hold the gems up */
-    platform = this.physics.add.staticGroup()
-    platform.create(VIEW.width / 2, VIEW.height-30, 'platformx384')
-    this.physics.add.collider(gems, platform)
+    
 }
 
 MAIN.update = function() {
